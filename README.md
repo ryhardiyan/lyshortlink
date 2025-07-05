4c345c468426caca3a271f910fe5e678d830941a
ryhardiyan <ryhardev@gmail.com>
2025-07-05 22:11:09 +0700

    main

---
+++ README.md
@@ -0,0 +1,2 @@
+# lyshortlink
+# lyshortlink
---
+++ public/sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = "lyshortlink-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/admin.html",
+  "/password.html",
+  "/styles/style.css",
+  "/manifest.json",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((res) => res || fetch(event.request))
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((names) =>
+      Promise.all(names.filter((n) => n !== CACHE_NAME).map((n) => caches.delete(n)))
+    )
+  );
+});
